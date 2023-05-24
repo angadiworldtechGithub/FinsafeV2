aa686e0b071685676f202ba6b6281d45282dd046
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-24 15:50:39 +0530

    dropdown changes

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import shortid from "shortid";
 import "./Dropdown.css";
 
+export const addMobileShow = (classString) => {
+  return classString + " mobile_show";
+};
+
 export default function Dropdown({
   isActive,
   setIsActive,
@@ -12,11 +16,11 @@ export default function Dropdown({
 }) {
   return (
     <div className="dropdown_text navbar_text_mobile">
-      <Link to={headerLink} className="navbar_link">
+      <Link to={headerLink} className="navbar_link" onClick={setIsActive}>
         {headerName}
       </Link>
       <MdArrowDropDown className="icon" />
-      <div className="dropdown">
+      <div className={isActive ? addMobileShow("dropdown") : "dropdown"}>
         <ul className="dropdown_list">
           {subHeaders.map((subHeader) => (
             <li key={shortid.generate()} className="sub_heading_text">
