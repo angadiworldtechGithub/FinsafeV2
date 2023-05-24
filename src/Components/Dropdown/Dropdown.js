4acc07e82cd92e793b55e162bdb2efccbf557cda
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-24 11:40:26 +0530

    added headerLink and formatting

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,24 +1,31 @@
-import { addHide } from "../Navbar/Navbar"
+import { addHide } from "../Navbar/Navbar";
 import { MdArrowDropDown } from "react-icons/md";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export default function Dropdown ({isActive, setIsActive, headerName, subHeaders}) {
-
-    return (
-    <>
-        <div className="navbar_text">
-                <Link to="/" className='navbar_link'>
-                    {headerName}
-                </Link>
-                <MdArrowDropDown className="icon" onClick={setIsActive}/>
-              </div>
-        
-              <div className={isActive ? 'dropdown' : addHide('dropdown')}>
-                  <ul>
-                    {subHeaders.map(subHeader => <li><Link to={subHeader.link} className='navbar_link'>{subHeader.title}</Link></li>)}
-                 </ul>
-        </div>
-    </>
-    )
+export default function Dropdown({
+  isActive,
+  setIsActive,
+  headerName,
+  headerLink,
+  subHeaders,
+}) {
+  return (
+    <div className="navbar_text">
+      <Link to={headerLink} className="navbar_link">
+        {headerName}
+      </Link>
+      <MdArrowDropDown className="icon" onClick={setIsActive} />
+      <div className={isActive ? "dropdown" : addHide("dropdown")}>
+        <ul className="dropdown_list">
+          {subHeaders.map((subHeader) => (
+            <li>
+              <Link to={subHeader.link} className="navbar_link">
+                {subHeader.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-  
