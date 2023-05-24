e46ba43acffdcd936d7eaf902dff27891aa8b345
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-24 14:04:25 +0530

    router changes and navbar fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+const Base = () => {
   return (
     <>
       <Header />
       <Navbar />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Base />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />;
     </>
   );
 }
