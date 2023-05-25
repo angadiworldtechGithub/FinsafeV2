b4b1d11a13e16270a86833c74600f3ed20f5522b
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-25 13:28:52 +0530

    added auth context

---
+++ src/Context/AuthContext/AuthContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import { auth as firebaseAuth } from "../../firebase";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext();
+
+// May have to change the AuthContext
+export function AuthContextProvider({ children }) {
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      if (user) {
+        setAuth(user);
+      }
+    });
+  });
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
