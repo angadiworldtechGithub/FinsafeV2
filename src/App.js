e9da25eb5d32f4ac7921a0baf901943ab436ebc9
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-25 13:15:23 +0530

    added authcontext and admin and dashboard pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
+import Admin from "./Pages/Admin";
+import Dashboard from "./Pages/Dashboard";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { AuthContextProvider } from "./Context/AuthContext";
 
 const Base = () => {
   return (
@@ -24,15 +27,23 @@ const router = createBrowserRouter([
         path: "",
         element: <Home />,
       },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/admin",
+        element: <Admin />,
+      },
     ],
   },
 ]);
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <RouterProvider router={router} />
-    </>
+    </AuthContextProvider>
   );
 }
 
