a03564bc1f10db4ff664eef474909a24d508d1df
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-26 16:59:55 +0530

    home fix

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,65 +4,6 @@ import "@splidejs/react-splide/css";
 import "./Home.css";
 import shortid from "shortid";
 
-<<<<<<< HEAD
-export default function Home() {
-  const ProjectList = [
-    {
-      title: "CFO Services",
-      image: "assets/images/3.png",
-      description:
-        "Freelancing Chief Financial Officer services CFO.Finsafe CFO services,strong financial growth just like large MNCs.",
-    },
-    {
-      title: "Loan Services",
-      image: "assets/images/24.jpg",
-      description:
-        "Finsafe maintains with banks and Financial institutions which helps to raise required growth capital for your organisation",
-    },
-    {
-      title: "Transaction Advisory",
-      image: "assets/images/23.jpg",
-      description:
-        "Finsafe experts will help you in drafting and vetting business agreements which help you enter into strong business relationships",
-    },
-    {
-      title: "Tax Planning",
-      image: "assets/images/22.jpg",
-      description:
-        "Our Tax Experts will help implement best Tax best planning for your organisation.",
-    },
-    {
-      title: "Statutory Compliances",
-      image: "assets/images/21.jpg",
-      description:
-        "Our experienced team ensures your organisation is always compliant (GST, RERA, Income tax and Companies act etc.)",
-    },
-    {
-      title: "Business Services",
-      image: "assets/images/20.jpg",
-      description:
-        "FINSAFE efficient process such as payroll, AP & AR will help reduce operations cost signficantly.",
-    },
-    {
-      title: "Financial planning",
-      image: "assets/images/19.jpg",
-      description:
-        "FINSAFE's Certified financial planners builds a robust plan to help achive your financial targets",
-    },
-    {
-      title: "Investment",
-      image: "assets/images/18.jpg",
-      description:
-        "Our relationship with large financial institutions ensure highest value loan at lowest possible returns	",
-    },
-    {
-      title: "Wealth Management services",
-      image: "assets/images/17.jpg",
-      description:
-        "Our team of Experts ensures your wealth, assets are preserved and passed on to Generations to come.	",
-    },
-  ];
-=======
 const ProjectList = [
   {
     title: "CFO Services",
@@ -119,7 +60,6 @@ const ProjectList = [
       "Our team of Experts ensures your wealth, assets are preserved and passed on to Generations to come.	",
   },
 ];
->>>>>>> 70427b4f3ad43d4db0ef8560754f0d5b24272d93
 
 export default function Home() {
   return (
