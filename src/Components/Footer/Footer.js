8a40b09b3132df8d12ec55398ad71456165502a2
harika <harika@angadiworldtech.com>
2023-05-26 11:07:54 +0530

    footer and service page changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -11,73 +11,48 @@ return(
      <div className='col'>
          <img src="assets/images/logo.png" className='footer_logo'/>
          <div className='col'>
-         {/*<form>
-             <i class=" far fa-envelope"></i>
-             <input type="email" placeholder="Enter your email id" required></input>
-             <botton type="submit"><i classname="fas fa-arrow-right"></i></botton>
-      </form>*/}
           <div className='social-icons'>
-                <i className="fab fa-facebook-f"></i>
-                <i className="fab fa-twitter"></i>
-                <i className="fab fa-whatsapp"></i>
-                <i className="fab fa-instagram"></i>
+               <Link to="/"><i className="fab fa-facebook-f"></i></Link>
+               <Link to="/"><i className="fab fa-twitter"></i></Link>
+               <Link to="/"><i className="fab fa-whatsapp"></i></Link>
+               <Link to="/"><i className="fab fa-instagram"></i></Link>
           </div>
      </div>
-     
-      </div>
+     </div>
        <div className='col'>
             <h3>Popular Services <div className='underline'><span></span></div></h3>
-            <ul>
-               <li><a href="">Take Expert Advice</a></li>
-               <li><a href="">Start Your Business</a></li>
-               <li><a href="">CFO Services</a></li>
-               <li><a href="">Accounts & Audit</a></li>
-               <li><a href="">Loans & Capital</a></li>
-               <li><a href="">Business Legal Services</a></li>
-               <li><a href="">Statutory Compliances</a></li>
-               <li><a href=""></a>Pesonal Services</li>
-      
-
+            <ul className='footer_list'>
+               <li><Link to="">Take Expert Advice</Link></li>
+               <li><Link to="">Start Your Business</Link></li>
+               <li><Link to="">CFO Services</Link></li>
+               <li><Link to="">Accounts & Audit</Link></li>
+               <li><Link to="">Loans & Capital</Link></li>
+               <li><Link to="">Business Legal Services</Link></li>
+               <li><Link to="">Statutory Compliances</Link></li>
+               <li><Link to=""></Link>Pesonal Services</li>
           </ul>
        </div>
        <div className='col'>
           <h3>COMPANY <div className='underline'><span></span></div></h3>
-          <ul>
-              <li><a href="">Home</a></li>
-              <li><a href="">About Us</a></li>
-              <li><a href="">Contact</a></li>
-        
-
+          <ul className='footer_list'>
+              <li><Link to="">Home</Link></li>
+              <li><Link to="">About Us</Link></li>
+              <li><Link to="">Contact</Link></li>
           </ul>
        </div>
        <div className='col'>
            <h3>Terms & Policies<div className='underline'><span></span></div></h3>
-           <ul>
-              <li><a href="">Privacy Policy</a></li>
-              <li><a href="">Terms & Conditions</a></li>
+           <ul className='footer_list'>
+              <li><Link to="">Privacy Policy</Link></li>
+              <li><Link to="">Terms & Conditions</Link></li>
           </ul>
        </div>
-
-      {/* <div className='col'>
-           {/*<form>
-               <i class=" far fa-envelope"></i>
-               <input type="email" placeholder="Enter your email id" required></input>
-               <botton type="submit"><i classname="fas fa-arrow-right"></i></botton>
-        </form>
-            <div className='social-icons'>
-                  <i className="fab fa-facebook-f"></i>
-                  <i className="fab fa-twitter"></i>
-                  <i className="fab fa-whatsapp"></i>
-                  <i className="fab fa-instagram"></i>
-            </div>
-    </div>*/}
-
    </div>
-   <hr></hr>
+   <div className="line_break"></div>
    <div className="copyright">
-   <p className="color_white">
-       Copyright © 2023 <a to="/index">Finsafe</a>
-   </p>
+    <p className="color_white">
+        Copyright © 2023 <Link to="/index">Finsafe</Link>
+    </p>
    </div>
 </div>
 
