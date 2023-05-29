c66309a564db4455df8fc0e6cf3552067f961516
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-05-29 11:00:57 +0530

    changes class to classname

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -21,7 +21,7 @@ export default function Contact() {
               marginBottom: "30px",
             }}
           >
-            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <i className="fa fa-map-marker" aria-hidden="true"></i>
           </div>
           <p>
             Address :<br></br> Opp : Court Complex,<br></br>(PVR Associates)
@@ -31,7 +31,7 @@ export default function Contact() {
 
         <div className="box_text1">
           <div>
-            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+            <i className="fa fa-envelope-o" aria-hidden="true"></i>
           </div>
           <p>
             <a href="mailto:support@finsafe.in">
@@ -42,7 +42,7 @@ export default function Contact() {
 
         <div className="box_text2">
           <div>
-            <i class="fa fa-mobile" aria-hidden="true"></i>
+            <i className="fa fa-mobile" aria-hidden="true"></i>
           </div>
           <p>
             <a href="tel:+91-90000-90000">
@@ -66,15 +66,15 @@ export default function Contact() {
                 Associates)<br></br>RAJAMPET-516115,YSR Kadapa Dist,A.P.
               </p>
             </address>
-            <div class="mail_box">
-              <div class="icon">
+            <div className="mail_box">
+              <div className="icon">
                 <i
-                  class="fa fa-envelope"
+                  className="fa fa-envelope"
                   aria-hidden="true"
                   style={{ fontsize: "25px" }}
                 ></i>{" "}
               </div>
-              <div class="text_mail">
+              <div className="text_mail">
                 <p>
                   <a href="mailto:support@finsafe.in"> support@finsafe.in</a>
                 </p>
