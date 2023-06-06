e86024e53b2f3fb5110378400142afaeb51cc28f
harika <harika@angadiworldtech.com>
2023-06-06 12:24:35 +0530

    changes in contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -12,100 +12,64 @@ export default function Contact() {
           alt="contact_us_image"
           width="100%"
         />
-      </div>
+    </div>
+
     <div className="contact_section">
     <div class="contact"  
     style={{
       paddingBottom: "0",
-      paddingTop: "0px",
+      paddingTop: "40px",
       marginBottom: "30px",
     }}>
-      <div class="contact1">ONE</div>
-      <div class="contact1">TWO</div>
-      <div class="contact1">THREE</div>
+
+      <div class="contact1">
+      
+      <center>
+      <i className="fa fa-map-marker" aria-hidden="true"></i> 
+       
+       <p><b style={{fontSize:'19px'}}>YSR Kadapa</b></p> 
+        Opp : Court Complex,<br></br>
+        (PVR Associates)
+        RAJAMPET-516115,<br></br>
+        YSR Kadapa Dist,<br></br>
+        Andhra Pradesh
+       </center>
+      
+      </div>
+      <div class="contact1">
+      <center>
+      <i className="fa fa-envelope-o" aria-hidden="true"></i>
+        <h2><b>finsafe@gmail.com</b></h2>
+      </center>
+      </div>
+      <div class="contact1">
+      <center><i className="fa fa-mobile" aria-hidden="true"></i>
+      <h2><b>+91 0000000000</b></h2>
+      </center>
+      </div>
    </div>
    </div>
 
+   <div className="contact"> 
+     
+       <div className="contact2">
+           <div className="row">
+              <h1>Talk to us!</h1>
+                  <p className="para">Feel Free to contact us any time. We will get back to you as soon as we can!.</p>
+                  <input type="text"  className="form-control form-group"placeholder="Name" />
+                  <input type="text"  className="form-control form-group" placeholder="Email" />
+                  <textarea   class="form-control form-group" placeholder="Message"></textarea>
+                  <button class="contact_form_submit">Send</button>
+                </div>
+       </div>
 
+      <div className="contact2">
+       <iframe src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Opp : Court Complex, (PVR Associates)  RAJAMPET-516115, YSR Kadapa Dist, Andhra Pradesh&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="400" frameborder="0" style={{border:'0'}}></iframe>
+     </div>
 
+  </div>
 
-     {/*<div style={{ position: "relative", backgroundColor: "#e6e6e6" }}>
-        <img
-          src="assets/images/contact.png"
-          alt="contact_us_image"
-          width="100%"
-        />
-      </div>
-
-      <div className="contact_section">
-        <div className="box_text">
-          <div
-            style={{
-              paddingBottom: "0",
-              paddingTop: "0px",
-              marginBottom: "30px",
-            }}
-          >
-            <h1 align="center"><i className="fa fa-map-marker" aria-hidden="true"></i></h1> 
-          </div>
-        </div>
-
-        <div className="box_text1">
-          <div>
-            <i className="fa fa-envelope-o" aria-hidden="true"></i>
-          </div>
-          <p>
-           
-  
-          </p>
-        </div>
-
-        <div className="box_text2">
-          <div>
-            <i className="fa fa-mobile" aria-hidden="true"></i>
-          </div>
-          <p>
-            <a href="tel:+91-90000-90000">
-              <h3>+91 0000000000</h3>
-            </a>
-          </p>
-        </div>
-          </div> */}
-
-      <div className="contact_section">
-        <div className="contact_text">
-          <h2>How can we help you today?</h2>
-        </div>
-
-        <div className="contact_text1">
-          <div className="box_txt">
-            <address>
-              <h3 style={{ fontWeight: "60px" }}>YSR Kadapa</h3>
-              <p>
-                Corporate Address: &nbsp;<br></br> Opp : Court Complex,(PVR
-                Associates)<br></br>RAJAMPET-516115,YSR Kadapa Dist,A.P.
-              </p>
-            </address>
-            <div className="mail_box">
-              <div className="icon">
-                <i
-                  className="fa fa-envelope"
-                  aria-hidden="true"
-                  style={{ fontsize: "25px" }}
-                ></i>{" "}
-              </div>
-              <div className="text_mail">
-                <p>
-                  <a href="mailto:support@finsafe.in"> support@finsafe.in</a>
-                </p>
-              </div>
-            </div>
-            <a href=" " target="_blank">
-              View Map
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+  </div>
+    
   );
 }
