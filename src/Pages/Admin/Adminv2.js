f7f920b3277556abb5c43932d871030dab1760e4
harika <harika@angadiworldtech.com>
2023-06-12 15:23:40 +0530

    change

--- src/Pages/Admin/Adminv2.js
+++ src/Pages/Admin/Adminv2.js
@@ -6,14 +6,30 @@ export default function Adminv2() {
         <h1>User Profile</h1>
         <ul class="flex-container">
           <li class="flex-item">
+            <label>User Name:</label>
             <input
-              style={{ background: "#e6e6e6" }}
               className="input-admin"
               type="text"
               placeholder="User Name"
             ></input>
           </li>
-          <li class="flex-item">Email :</li>
+          <li class="flex-item">
+          <label>Email:</label>
+          <input
+            className="input-admin"
+            type="Email"
+            placeholder="User Email"
+          ></input>
+        </li>
+
+        <li class="flex-item">
+        <label>Phone Number:</label>
+        <input
+          className="input-admin"
+          type="phonenumber"
+          placeholder="Phone Number"
+        ></input>
+       </li>
         </ul>
       </div>
     );
