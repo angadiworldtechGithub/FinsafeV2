842bd37d298f1d6aa024b96b44636116d4197b4a
harika <harika@angadiworldtech.com>
2023-06-14 22:32:34 +0530

    changes

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,3 +1,22 @@
+import "./Admin.css"
+import { BsChevronRight } from "react-icons/bs";
 export default function Admin() {
-  return <h1>Super Admin</h1>;
+  return (
+     <div>
+     <div className="company-container">
+     <p className="welcome">Welcome!</p>
+     <div className="company-left">
+       <p className="company-header">List of Companies</p>
+       <button className="company-button">Company 1</button><i className="company-icon"><BsChevronRight/></i>
+       <button className="company-button">Company 2</button><i className="company-icon"><BsChevronRight /></i>
+       <button className="company-button">Company 2</button><i className="company-icon"><BsChevronRight/></i>
+      </div>
+     <div className="company-right">
+     <p className="company-header">List of  Individuals</p>
+      <button className="company-button">Individuals 1</button><i className="company-icon"><BsChevronRight/></i>
+      <button className="company-button">Individuals 2</button><i className="company-icon"><BsChevronRight/></i>
+     </div>
+    </div>
+    </div>
+    );
 }
