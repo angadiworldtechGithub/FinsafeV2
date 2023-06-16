e6ead8e2acd6652ba28e8bb6f62857ba78c5f3b6
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-06-16 17:44:30 +0530

    updates

--- src/Pages/Dashboard/YearFileInput.js
+++ src/Pages/Dashboard/YearFileInput.js
@@ -9,75 +9,63 @@ export default function YearFileInput({ fileInput }) {
     <>
       <h4 className="date-button">{fileInput.name} Files</h4>
       <div className="income-flex">
-         <div>
-               <label className="label-flex"> Choose Date - </label>
-               <input type="date" />
-         </div>
-         <div>
-              <label className="label-flex">Upload File - </label>
-              <input type="file" />
-         </div>
-         <div>
-            <button className="button-upload">Upload</button>
-         </div>
+        <div>
+          <label className="label-flex"> Choose Date - </label>
+          <input type="date" />
+        </div>
+        <div>
+          <label className="label-flex">Upload File - </label>
+          <input type="file" />
+        </div>
+        <div>
+          <button className="button-upload">Upload</button>
+        </div>
       </div>
-      <table className="Year">
-         <tr>
-      <div>
-        <label> Choose Date - </label>
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Upload File</label>
-        <input type="file" ref={docRef} />
-      </div>
-      <div>
-        <button className="button-upload">Upload</button>
-      </div>
+
       <table>
         <tr>
           <th>Date of Document</th>
           <th>Download</th>
         </tr>
         <tr>
-         {/* {fileInput.documents.map((document) => {
-            return (
-              <>
-                <td>{document.date}</td>
-                <td>
-                  <a
-                    href={document.fileDownloadUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <BsDownload />
-                  </a>
-                </td>
-              </>
-            );
-          })}*/}
-           <td>21/08/2023</td>
-           <td><i><GrDownload/></i></td>
+          <td>21/08/2023</td>
+          <td>
+            <i>
+              <GrDownload />
+            </i>
+          </td>
         </tr>
         <tr>
-           <td>22/08/2023</td>
-           <td><i><GrDownload/></i></td>
+          <td>22/08/2023</td>
+          <td>
+            <i>
+              <GrDownload />
+            </i>
+          </td>
         </tr>
         <tr>
-           <td>23/08/2023</td>
-           <td><i><GrDownload/></i></td>
+          <td>23/08/2023</td>
+          <td>
+            <i>
+              <GrDownload />
+            </i>
+          </td>
         </tr>
         <tr>
-           <td>24/08/2023</td>
-           <td><i><GrDownload/></i></td>
+          <td>24/08/2023</td>
+          <td>
+            <i>
+              <GrDownload />
+            </i>
+          </td>
         </tr>
         <tr>
-           <td>25/08/2023</td>
-           <td><i><GrDownload/></i></td>
+          <td>25/08/2023</td>
+          <td>
+            <i>
+              <GrDownload />
+            </i>
+          </td>
         </tr>
       </table>
     </>
