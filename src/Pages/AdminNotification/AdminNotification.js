8b20f1386308b9bcfa663219692d5211a5052f1c
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-06-22 17:46:51 +0530

    updates

--- src/Pages/AdminNotification/AdminNotification.js
+++ src/Pages/AdminNotification/AdminNotification.js
@@ -1,27 +1,35 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AiOutlineLoading } from "react-icons/ai";
-import { NOTIF_COLL_NAME, USER_NOTIF_COLL_NAME } from "../../constants";
+import {
+  NOTIF_COLL_NAME,
+  USER_NOTIF_COLL_NAME,
+  ADMIN_EMAILS,
+} from "../../constants";
 import { addData } from "../../API/createDoc";
 import { getAllDocs } from "../../API/readDoc";
 import { showLoading } from "react-global-loading";
 import { sortDateList } from "../utilities";
+import { AuthContext } from "../../Context/AuthContext";
 
 export default function AdminNotification() {
   const [notifications, setNotifications] = useState([]);
   const [message, setMessage] = useState("");
   const [sending, setSending] = useState(false);
+  const { auth } = useContext(AuthContext);
   useState(() => {
-    (async () => {
-      showLoading(true);
-      const notifications = await getAllDocs(NOTIF_COLL_NAME);
-      notifications.sort(sortDateList);
-      setNotifications(
-        notifications.map((notification) => {
-          return { ...notification, readBy: [] };
-        })
-      );
-      showLoading(false);
-    })();
+    if (auth) {
+      (async () => {
+        showLoading(true);
+        const notifications = await getAllDocs(NOTIF_COLL_NAME);
+        notifications.sort(sortDateList);
+        setNotifications(
+          notifications.map((notification) => {
+            return { ...notification, readBy: [] };
+          })
+        );
+        showLoading(false);
+      })();
+    }
   }, []);
   const sendHandler = async () => {
     setSending(true);
@@ -31,6 +39,16 @@ export default function AdminNotification() {
     setMessage("");
     setSending(false);
   };
+
+  if (!auth || (auth && !ADMIN_EMAILS.includes(auth.email))) {
+    return (
+      <div className="dashboard_container">
+        <h1>Admin Services</h1>
+        <h1>Please login with a admin account to view this page.</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div
